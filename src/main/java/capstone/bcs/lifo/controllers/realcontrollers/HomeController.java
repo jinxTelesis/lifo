611daa862b5b32c013cc583749dda325b09906ab
Dre <dremon7@hotmail.com>
2019-03-01 22:42:43 -0500

    refactored mapping

--- src/main/java/capstone/bcs/lifo/controllers/realcontrollers/HomeController.java
+++ src/main/java/capstone/bcs/lifo/controllers/realcontrollers/HomeController.java
@@ -3,12 +3,13 @@ package capstone.bcs.lifo.controllers.realcontrollers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@RequestMapping("realprojectpages")
+@Controller // == applies to all of them
 public class HomeController {
 
     @RequestMapping("/home") // == options in browser that map to the return ==
     public String getPage(){
-        return "realprojectpages/home"; // == page it directs to. You don't need to make the 3 different options ==
+        return "home"; // == page it directs to. You don't need to make the 3 different options ==
         // == the page it directs to will be in resources under templates with the return {fill in return name}.html ==
     }
 }
--- src/main/java/capstone/bcs/lifo/controllers/realcontrollers/ItemAdminController.java
+++ src/main/java/capstone/bcs/lifo/controllers/realcontrollers/ItemAdminController.java
@@ -4,12 +4,14 @@ package capstone.bcs.lifo.controllers.realcontrollers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@RequestMapping("realprojectpages")
 @Controller
 public class ItemAdminController {
 
     @RequestMapping("/itemadmin") // == options in browser that map to the return ==
     public String getPage(){
-        return "realprojectpages/itemadmin"; // == page it directs to. You don't need to make the 3 different options ==
+        return "itemadmin"; // == page it directs to. You don't need to make the 3 different options ==
         // == the page it directs to will be in resources under templates with the return {fill in return name}.html ==
     }
 }
