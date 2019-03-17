63fa33626920dbd119b628704132a66982fceb08
Dre <dremon7@hotmail.com>
2019-03-17 18:06:06 -0400

    mashup of the two pages

--- src/main/java/capstone/bcs/lifo/controllers/IndexController.java
+++ src/main/java/capstone/bcs/lifo/controllers/IndexController.java
@@ -1,6 +1,9 @@
 package capstone.bcs.lifo.controllers;
 
+import capstone.bcs.lifo.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -8,9 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     // == the main page should have /.html
-    @RequestMapping({"/","/index","/.html","/index.html"})
+    //@RequestMapping({"/","/index","/.html","/index.html"})
     public String index(){
-        return "index2";
+        return "index3";
+    }
+
+    private ProductService productService;
+
+    @Autowired // intention
+    IndexController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @RequestMapping({"/","/index","/.html","/index.html"})
+    public String getIndex(Model model){
+        model.addAttribute("products", productService.getProducts());
+        return "index3";
     }
 
 }
---
+++ src/main/java/capstone/bcs/lifo/controllers/ProductControllerNOTPRODUCTS.java
@@ -0,0 +1,35 @@
+package capstone.bcs.lifo.controllers;
+
+import capstone.bcs.lifo.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class ProductControllerNOTPRODUCTS {
+
+    private ProductService productService;
+
+
+    @Autowired // intention only
+    ProductControllerNOTPRODUCTS(ProductService productService){
+        this.productService = productService;
+    }
+
+
+
+    //@RequestMapping("/product")
+    public String getProduct(){
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProductById(@PathVariable Integer id, Model model){
+
+        model.addAttribute("product", productService.findById(Integer.toUnsignedLong(id)));
+        return "product";
+    }
+}
