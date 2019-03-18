010d654003c2b3487163fc97bf26d5ab67c57626
Dre <dremon7@hotmail.com>
2019-03-18 17:18:01 -0400

    Merge remote-tracking branch 'remotes/origin/workingPreMerge' into dresCorrectVersion
    # Conflicts:
    #	src/main/java/capstone/bcs/lifo/controllers/IndexController.java
    #	src/main/resources/templates/index2.html

--- src/main/java/capstone/bcs/lifo/services/OrderDetailService.java
+++ src/main/java/capstone/bcs/lifo/services/OrderDetailService.java
@@ -1,15 +1,15 @@
 package capstone.bcs.lifo.services;
 
-import capstone.bcs.lifo.model.Account;
 import capstone.bcs.lifo.model.OrderDetail;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderDetailService {
 
     List<OrderDetail> getProducts();
 
-    Account findById(Long l);
+    Optional<OrderDetail> findById(Long l);
 
     void deleteById(Long idToDelete);
 }
--- src/main/java/capstone/bcs/lifo/services/OrderDetailServiceImpl.java
+++ src/main/java/capstone/bcs/lifo/services/OrderDetailServiceImpl.java
@@ -1,11 +1,10 @@
 package capstone.bcs.lifo.services;
 
-import capstone.bcs.lifo.model.Account;
+import capstone.bcs.lifo.Exceptions.NotFoundException;
 import capstone.bcs.lifo.model.OrderDetail;
 import capstone.bcs.lifo.repositories.OrderDetailRepository;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -28,30 +27,19 @@ public class OrderDetailServiceImpl implements OrderDetailService{
     }
 
     @Override
-    public Account findById(Long l) {
+    public Optional<OrderDetail> findById(Long l) {
         Optional<OrderDetail> orderDetailOptional = orderDetailRepository.findById(l);
-        return null;
+
+        if(!orderDetailOptional.isPresent()){
+            throw new NotFoundException();
+        }
+        return orderDetailOptional;
     }
 
     @Override
     public void deleteById(Long idToDelete) {
-
+        orderDetailRepository.deleteById(idToDelete);
     }
-
-//
-//    @Override
-//    public Account findById(Long l) {
-//        Optional<Account> accountOptional = accountRepository.findById(l);
-//
-//        if(!accountOptional.isPresent()){
-//            throw new NotFoundException();
-//        }
-//        return accountOptional.get();
-//    }
-//
-//    @Override
-//    public void deleteById(Long idToDelete) {
-//        accountRepository.deleteById(idToDelete);
-//    }
+    
 
 }
