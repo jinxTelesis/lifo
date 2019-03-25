aedd97d1d9f5b5b86df2eba496cf73d131f9eb96
hernhm <hernhm@farmingdale.edu>
2019-03-24 23:05:23 -0400

    Demo for 03/25

--- src/main/java/capstone/bcs/lifo/LifoApplication.java
+++ src/main/java/capstone/bcs/lifo/LifoApplication.java
@@ -2,11 +2,17 @@ package capstone.bcs.lifo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LifoApplication {
+public class LifoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LifoApplication.class, args);
+    }
+
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder builder){
+        return builder.sources(LifoApplication.class);
     }
 }
