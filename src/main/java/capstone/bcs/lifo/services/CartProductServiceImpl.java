e77c90f53fa425a5cd8597be48500335c4386f4a
Andre Lussier <Dremon7@hotmail.com>
2019-04-07 20:51:43 -0400

    added Cart Service and CartProducts Service

--- src/main/java/capstone/bcs/lifo/services/CartProductServiceImpl.java
+++ src/main/java/capstone/bcs/lifo/services/CartProductServiceImpl.java
@@ -1,26 +1,45 @@
 package capstone.bcs.lifo.services;
 
+
+import capstone.bcs.lifo.Exceptions.NotFoundException;
 import capstone.bcs.lifo.model.CartProducts;
+import capstone.bcs.lifo.repositories.CartProductsRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartProductServiceImpl implements CartProductsService {
 
+    // this service needs to be finished 
 
-    @Override
-    public List<CartProducts> getCartProducts() {
-        return null;
+    private final CartProductsRepository cartProductsRepository;
+
+    public CartProductServiceImpl(CartProductsRepository cartProductsRepository) {
+        this.cartProductsRepository = cartProductsRepository;
     }
 
     @Override
-    public CartProducts findById(Long l) {
-        return null;
+    public List<CartProducts> getCartProducts() {
+        List<CartProducts> cartProductsList= new ArrayList<>();
+        cartProductsRepository.findAll().iterator().forEachRemaining(cartProductsList::add);
+        return cartProductsList;
+    }
+
+    @Override
+    public CartProducts findById(Long longId) {
+        Optional<CartProducts> cartProductsOptional = cartProductsRepository.findById(longId);
+        if(!cartProductsOptional.isPresent()){
+            throw new NotFoundException();
+        }
+        return cartProductsOptional.get();
     }
 
     @Override
     public void deleteById(Long idToDelete) {
-
+        cartProductsRepository.deleteById(idToDelete);
     }
+
 }
