ed920151a5bc3b1ceb11068cccea789faa62b6fb
Andre Lussier <Dremon7@hotmail.com>
2019-04-09 17:29:08 -0400

    patch for product pages

--- src/main/java/capstone/bcs/lifo/controllers/ProductsController.java
+++ src/main/java/capstone/bcs/lifo/controllers/ProductsController.java
@@ -34,4 +34,61 @@ public class ProductsController {
         return "products";
     }
 
+    // == the javascript drops out when the path variable is passed in == investigating changing the thyemeleaf ==
+    // == this is temporary hack solution to the problem that will be removed later ==
+    // == the products should just be mapped with a category but the javascript issue is why this is done this way
+    // == hopefully this will be removed to just /products/{productCat} soon ==
+
+    @RequestMapping("/products_accessories")
+    public String getAccessories(Model model)
+    {
+        model.addAttribute("products", productService.getProductsByCategory(0));
+        return "products_accessories";
+    }
+
+    @RequestMapping("/products_appliances")
+    public String getAppliances(Model model)
+    {
+        model.addAttribute("products",productService.getProductsByCategory(1));
+        return "products_appliances";
+    }
+
+    @RequestMapping("/products_cleaning")
+    public String getCleaning(Model model)
+    {
+        model.addAttribute("products",productService.getProductsByCategory(2));
+        return "products_cleaning";
+    }
+
+    @RequestMapping("/products_hardware")
+    public String getHardware(Model model)
+    {
+        model.addAttribute("products",productService.getProductsByCategory(3));
+        return "products_hardware";
+    }
+
+    @RequestMapping("/products_health")
+    public String getHealth(Model model)
+    {
+        model.addAttribute("products",productService.getProductsByCategory(4));
+        return "products_health";
+    }
+
+    @RequestMapping("/products_home")
+    public String getHome(Model model)
+    {
+        model.addAttribute("products",productService.getProductsByCategory(5));
+        return "products_home";
+    }
+
+    @RequestMapping("/products_toys")
+    public String getToys(Model model)
+    {
+        model.addAttribute("products",productService.getProductsByCategory(6));
+        return "products_toys";
+    }
+
+
+
+
 }
