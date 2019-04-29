81d6528ed04f9a577af958309ad67e9bba9305dd
Andre Lussier <Dremon7@hotmail.com>
2019-04-28 22:04:52 -0400

    don't use this branch

---
+++ src/main/java/capstone/bcs/lifo/bootstrap/LoadDatabase.java
@@ -0,0 +1,23 @@
+package capstone.bcs.lifo.bootstrap;
+
+import capstone.bcs.lifo.model.Employee;
+
+import capstone.bcs.lifo.repositories.EmployeeRespostiory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRespostiory repository){
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Billo Baggins","burglar")));
+            log.info("Preloading" + repository.save(new Employee("Frodo Baggins","theif")));
+        };
+    }
+
+}
---
+++ src/main/java/capstone/bcs/lifo/controllers/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package capstone.bcs.lifo.controllers;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException(Long id) {
+
+    }
+}
