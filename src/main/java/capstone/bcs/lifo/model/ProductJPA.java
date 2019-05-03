6462b253820eedb362a79d1075f237b6c872a1b8
Andre Lussier <Dremon7@hotmail.com>
2019-05-03 18:02:51 -0400

    TESTVERSIONDONTPULL

---
+++ src/main/java/capstone/bcs/lifo/model/ProductJPA.java
@@ -0,0 +1,47 @@
+package capstone.bcs.lifo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class ProductJPA {
+
+    private @Id
+    @GeneratedValue
+    Long id;
+    private String firstName;
+    private String lastName;
+    private String role;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
