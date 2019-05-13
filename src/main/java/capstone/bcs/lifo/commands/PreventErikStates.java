f090ab6af4f8df01ff9a1fce43306a98abd4cda3
hernhm <hernhm@farmingdale.edu>
2019-05-13 03:41:44 -0400

    5-13 3:39 updates

---
+++ src/main/java/capstone/bcs/lifo/commands/PreventErikStates.java
@@ -0,0 +1,11 @@
+package capstone.bcs.lifo.commands;
+
+public enum PreventErikStates {
+    PREVENT_EMAIL,
+    PREVENT_USERNAME,
+    PREVENT_PASSWORD,
+    PREVENT_PHONENUMBER,
+    PREVENT_ADDRESS,
+    PREVENT_CITY,
+    PREVENT_ZIP,
+}
